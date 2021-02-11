e9ffd47fe103a6ddca83594d7c0dee67e885c477
benoittirot <benoit.tirot2020@campus-eni.fr>
2021-02-11 15:58:41 +0100

    etatController

--- src/Controller/EtatController.php
+++ src/Controller/EtatController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Campus;
 use App\Entity\Etat;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
@@ -16,30 +17,12 @@ class EtatController extends AbstractController
      */
     public function addEtat(EntityManagerInterface $entityManager): Response
     {
-        $creationEtat = new Etat();
-        $creationEtat->setLibelle('Créée');
-        $entityManager->persist($creationEtat);
-
-        $ouvertureEtat= new Etat();
-        $ouvertureEtat->setLibelle('Ouverture');
-        $entityManager->persist($ouvertureEtat);
-
-        $clotureEtat= new Etat();
-        $clotureEtat->setLibelle('Clôturée');
-        $entityManager->persist($clotureEtat);
-
-        $enCoursEtat= new Etat();
-        $enCoursEtat->setLibelle('Activité en cours');
-        $entityManager->persist($enCoursEtat);
-
-        $passeEtat= new Etat();
-        $passeEtat->setLibelle('Passée');
-        $entityManager->persist($passeEtat);
-
-        $annuleEtat= new Etat();
-        $annuleEtat->setLibelle('Annulée');
-        $entityManager->persist($annuleEtat);
-
+       $etat = ['Créée', 'Ouverture', 'Clôturée', 'Activité en cours', 'Passée', 'Annulée'];
+        foreach($etat as $state){
+            $creationEtat = new Etat();
+            $creationEtat->setLibelle($state);
+            $entityManager->persist($creationEtat);
+        }
         $entityManager->flush();
 
         return $this->render('etat/index.html.twig',[
