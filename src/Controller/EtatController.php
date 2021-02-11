ccd365b0bbc89284d7b9f50ca93d06ac64eb99fe
benoittirot <benoit.tirot2020@campus-eni.fr>
2021-02-11 11:29:05 +0100

    relation entre Etat et Sortie + ajout d'états en bdd

---
+++ src/Controller/EtatController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Etat;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EtatController extends AbstractController
+{
+    /**
+     * @Route("/etats/add", name="etats_add")
+     */
+    public function addEtat(EntityManager $entityManager): Response
+    {
+        $creationEtat = new Etat();
+        $creationEtat->setLibelle('Créée');
+        $entityManager->persist($creationEtat);
+
+        $ouvertureEtat= new Etat();
+        $ouvertureEtat->setLibelle('Ouverture');
+        $entityManager->persist($ouvertureEtat);
+
+        $clotureEtat= new Etat();
+        $clotureEtat->setLibelle('Clôturée');
+        $entityManager->persist($clotureEtat);
+
+        $enCoursEtat= new Etat();
+        $enCoursEtat->setLibelle('Activité en cours');
+        $entityManager->persist($enCoursEtat);
+
+        $passeEtat= new Etat();
+        $passeEtat->setLibelle('Passée');
+        $entityManager->persist($passeEtat);
+
+        $annuleEtat= new Etat();
+        $annuleEtat->setLibelle('Annulée');
+        $entityManager->persist($annuleEtat);
+
+        $entityManager->flush();
+
+        return new Response('ok');
+    }
+}
