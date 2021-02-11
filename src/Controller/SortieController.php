9ac5d14e306479c95c2487b7d3a2d4fe8f4a009a
benoittirot <benoit.tirot2020@campus-eni.fr>
2021-02-11 16:47:28 +0100

    sortie controller

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,19 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SortieController extends AbstractController
 {
     /**
-     * @Route("/sortie", name="sortie")
+     * @IsGranted("ROLE_USER")
+     * @Route("/sorties/create", name="sortie_create")
      */
-    public function index(): Response
+    public function createSorties(
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response
     {
-        return $this->render('sortie/index.html.twig', [
-            'controller_name' => 'SortieController',
+        //création de l'instance sortie vide associée au formulaire
+        $sortie = new Sortie();
+
+        $user = $this->getUser();
+
+
+
+
+        return $this->render('sortie/create.html.twig', [
+
         ]);
     }
 }
